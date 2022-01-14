6a484faf076d19970d872c9c1e7a070b8e21d355
Rishabh <itsindrajput@gmail.com>
2022-01-14 18:33:25 +0530

    to github

---
+++ Welcome To Kisan Dwar.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>| Kisan Dwar | Home Page |</title>
+<!--Meta tags -->
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<!--External CSS-->
+<link rel="stylesheet" href="Css_Files/Kisan Dwar Home Page.css" type="text/css"/>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+
+
+<body>
+<!--Naviagation Bar-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="navbarstyle">
+<div>
+<a class="navbar-brand" id="actives" href="Welcome To Kisan Dwar.php"><i class="fa fa-home"> HOME </i></a>
+</div>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item dropdown">
+<div class="btn-group" id="dropmenu">
+<button type="button" class="btn btn-warning"> EXPLORE INVENTORY </button>
+<button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<span class="sr-only">Toggle Dropdown</span>
+</button>
+<div class="dropdown-menu">
+<a class="dropdown-item explore" href="#">Dietry Needs.</a>
+<a class="dropdown-item explore" href="#">Food Production.</a>
+<a class="dropdown-item explore" href="#">Warehouse Layout.</a>
+<a class="dropdown-item explore" href="#">Storage Equipment System.</a>
+<a class="dropdown-item explore" href="#">Pulse Nitrogen Fixation.</a>
+<div class="dropdown-divider explore"></div>
+<a class="dropdown-item explore" href="#">Components Of Food Security.</a>
+<a class="dropdown-item explore" href="#">About Multi-Layer Farming.</a>
+</div>
+</div>
+</li>
+<li class="nav-item active">
+<form class="form-inline my-2 my-lg-0">
+<input class="form-control mr-sm-2" type="search"  placeholder="Enter Your District / State." aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" id="actives" type="submit">SEARCH</button>
+</form>
+</li>
+</ul>
+<div>
+<a  class="navbar-brand" id="actives" href="Login.html">LOG IN</a>
+<a  class="navbar-brand" id="actives" href="Signup.html">SIGN UP</a>
+</div>
+</div>
+</nav>
+
+
+
+<header>
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+<h1 class="display-7">WELCOME TO KISAN DWAR PORTAL</h1>
+<h4>| WE CARE YOU ARE NOT ALONE |</h4>
+<b>OUR GOAL IS TO DELIVER SUFFICIENT | SAFE | AFFORDABLE | & NUTRITIOUS FOOD.</b>
+</div>
+</div>
+</header>
+
+
+
+<div class="row">
+<div class="list col-4 col-sm-5 order-sm-last col-md-2 container float-left">
+<nav>
+<ul class="mainmenu">
+<li><a href="#">| Food Security [F.S] |</a>
+<ul class="submenu">
+<li><a href="#">Food Access.</a></li>
+<li><a href="#">Food Stability.</a></li>
+<li><a href="#">Food Utilization.</a></li>
+<li><a href="#">Food Availability.</a></li>
+</ul>
+</li>
+<li><a href="Gallery.html">Gallery</a>
+</li>
+<li><a href="#">F.C.I & M.S.P</a>
+<ul class="submenu">
+<li><a href="#">M.S.P Policies.</a></li>
+<li><a href="#">F.C.I Guidelines.</a></li>
+<li><a href="#">M.S.P Rules/Districts.</a></li>
+</ul>
+</li>
+<li><a href="#">Supply Chain</a>
+<ul class="submenu">
+<li><a href="#">Adv. Equipments.</a></li>
+<li><a href="#">Chain Management.</a></li>
+<li><a href="#">Incr. Productivity.</a></li>
+</ul>
+</li>
+<li><a href="#">Buffer Stock</a>
+<ul class="submenu">
+<li><a href="#">Increase Production.</a></li>
+<li><a href="#">Food Storage.</a></li>
+<li><a href="#">Hermetic Storage.</a></li>
+</ul>
+</li>
+<li><a href="#">Agri Investments</a>
+<ul class="submenu">
+<li><a href="#">On Storage Units.</a></li>
+<li><a href="#">On Warehouses.</a></li>
+<li><a href="#">Supply Chain.</a></li>
+</ul>
+</li>
+<li><a href="#">Farm Techniques</a>
+<ul class="submenu">
+    <li><a href="#">Irrigation Technique.</a></li>
+<li><a href="#">Greenhouse Farming.</a></li>
+<li><a href="#">Multilayer Farming.</a></li>
+<li><a href="#">Storage Techniques.</a></li>
+</ul>
+</li>
+<li><a href="#">Commodity Market</a>
+<ul class="submenu">
+<li><a href="#">Investors To Trade.</a></li>
+<li><a href="#">At The Market.</a></li>
+<li><a href="#">About Commodity Market.</a></li>
+</ul>
+</li>
+<li><a href="#">Cropping Pattern</a>
+<ul class="submenu">
+<li><a href="#">Land Productivity.</a></li>
+<li><a href="#">InterCropping.</a></li>
+<li><a href="#">Crop Rotation.</a></li>
+<li><a href="#">Mixed Cropping.</a></li>
+</ul>
+</li>
+</nav>
+</div>
+
+
+
+
+
+<div class="col-8 col-sm-7 order-sm-last col-md-10 float-right">
+<!--Image Scroll Function Beigns-->
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner">
+<div class="carousel-item active">
+<!--First Two Set of images-->
+
+<div class="card-deck">
+<div class="card">
+<img src="images/Kisan_Dwar_Home_Page.png" class="card-img-top" alt="Dr. Naresh Nagrath Profile Image">
+</div>
+
+<div class="card">
+<img src="images/Portal Home Page .jpg" class="card-img-top" alt="Dr. R.D Mukhija Profile Image">
+</div>
+</div>
+<!--End of First Two Set of images-->
+</div>
+<div class="carousel-item">
+
+<!--Second Two Set of images-->
+
+<div class="card-deck">
+<div class="card">
+<img src="images/Contact Page.jpg" class="card-img-top" alt="Profile Image">
+</div>
+<div class="card">
+<img src="images/Besides Contact Page.jpg" class="card-img-top" alt="Dr. Batra's Profile Image">
+</div>
+</div>
+</div>
+<!--End of Second Two Set of images-->
+
+<div class="carousel-item">
+<div class="card-deck">
+<div class="card">
+<img src="images/Map Of India.jpg" class="card-img-top" alt="Dr. Anil Malhotra Profile Image">
+</div>
+<div class="card">
+<img src="images/Besides Map Of India.jpg" class="card-img-top" alt="Dr. Gaurav Mukhija Profile Image">
+</div>
+</div>
+</div>
+</div>
+
+
+<!--End of Image Scroll Function Beigns-->
+
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+</div>
+</div>
+
+</div>
+</div>
+<br>
+<footer class="footer">
+<div class="container">
+<div class="row">             
+<div class="col-4 offset-1 col-sm-2">
+<ul class="list-unstyled">
+<li><a href="Feedback.html"><b>FEEDBACK</b></a></li> 
+<li><a href="About us.html"><b>ABOUT_US</b></a></li>
+<li><a href="Help.html"><b>HELP & SUPPORT</b></a></li>
+<li><a href="Privacy Policy.html"><b>PRIVACY POLICY</b></a></li>
+<li><a href="TermsOfUse.html"><b>TERMS OF USE</b></a></li>
+</ul>
+</div>
+<div class="col-7 col-sm-5 ">
+<a href="Contact us.html"><b>CONTACT_US</b></a>
+<address class="rish">
+I.E.T Khandari Agra,<br>
+Uttar-Pradesh, 282002. <br>
+<i class="fa fa-phone fa-lg"></i>: +91 6377022729<br>
+<i class="fa fa-fax fa-lg"></i>: +91 7080421805<br>
+<i class="fa fa-envelope fa-lg"></i>: 
+<a href="mailto:support_kisandwarportal@gmail.com">support_kisandwarportal@gmail.com</a>
+</address>
+</div>
+<div class="col-12  col-sm-4 align-self-center">
+<div class="text-cente icon-bar">
+<a href="mailto:Kisandwarportal@gmail.com"><i class="fa-2x fa fa-envelope"></i></a>
+<a href="https://twitter.com/DwarKissan?s=08"><i class="fa-2x fa fa-twitter"></i></a>
+<a href="https://github.com/Er-Rishabh-Singh/Rishabh-Singh-Profile"><i class="fa-2x fa fa-github"></i></a>
+<a href="#"><i class="fa-2x fa fa-bell"></i></a>
+<a href="#"><i class="fa-2x fa fa-share-alt"></i></i></a>
+<a href="#"><i class="fa-2x fa fa-facebook"></i></a>
+<a href="https://www.linkedin.com/in/rishabhsingh543"><i class="fa-2x fa fa-linkedin"></i></a>
+<a href="https://instagram.com/dwarkisan?igshid=w5tc5wehmlrz"><i class="fa-2x fa fa-instagram"></i></a>
+</div>
+</div>
+</div>
+<div class="row justify-content-center">             
+<div class="col-auto">
+<legend class="copyright"> |Kisan Dwar &copy 2021 Pvt Ltd &reg All Right Reserved.|<br />|View Our Tearms Of Use &amp; Privacy Policy.|</legend> 
+</div>
+</div>
+</div>
+</footer>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
